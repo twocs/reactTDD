d25031dd55b8bf9fdfe1a131e27d114acea29054
Tom Anderson <twocs@users.noreply.github.com>
2020-05-24 16:10:35 +0930

    Refactor to auto-construct Appointment container before each test

--- test/Appointment.test.js
+++ test/Appointment.test.js
@@ -5,10 +5,13 @@ import { Appointment } from '../src/Appointment';
 
 let customer, container;
 
+beforeEach(() => {
+    container = document.createElement('div');
+})
+
 describe('Appointment', () => {
   it('renders the customer\'s first name', () => {
       customer = { firstName: 'Tom' };
-      container = document.createElement('div');
       
       ReactDOM.render(<Appointment customer={ customer } />, container);
       expect(container.textContent).toMatch('Tom');
@@ -18,7 +21,6 @@ describe('Appointment', () => {
 describe('Appointment', () => {
     it('renders the customer\'s first name', () => {
         customer = { firstName: 'Michael' };
-        container = document.createElement('div');
       
         ReactDOM.render(<Appointment customer={ customer } />, container);
         expect(container.textContent).toMatch('Michael');
