343889cf97249021b7d751e2f66500a052c84bd4
Tom Anderson <twocs@users.noreply.github.com>
2020-05-24 16:30:50 +0930

    add (and run) lint

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # reactTDD
+
+https://www.sitepoint.com/premium/books/mastering-react-test-driven-development/read/1/k7fe6ep3
--- test/Appointment.test.js
+++ test/Appointment.test.js
@@ -1,30 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-import { Appointment } from '../src/Appointment';
+import { Appointment } from "../src/Appointment";
 
 let customer, container;
 
 beforeEach(() => {
-    container = document.createElement('div');
-})
+    container = document.createElement("div");
+});
 
 const render = component => ReactDOM.render(component, container);
 
-describe('Appointment', () => {
-  it('renders the customer\'s first name', () => {
-      customer = { firstName: 'Tom' };
-      
-      render(<Appointment customer={ customer } />);
-      expect(container.textContent).toMatch('Tom');
-  });
-});
-
-describe('Appointment', () => {
-    it('renders the customer\'s first name', () => {
-        customer = { firstName: 'Michael' };
+describe("Appointment", () => {
+    it("renders the customer's first name", () => {
+        customer = { firstName: "Tom" };
       
         render(<Appointment customer={ customer } />);
-        expect(container.textContent).toMatch('Michael');
+        expect(container.textContent).toMatch("Tom");
     });
-  });
+});
+
+describe("Appointment", () => {
+    it("renders the customer's first name", () => {
+        customer = { firstName: "Michael" };
+      
+        render(<Appointment customer={ customer } />);
+        expect(container.textContent).toMatch("Michael");
+    });
+});
