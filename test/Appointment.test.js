387a0055ef7694799b1a4e0be5b770cec71f51b4
Tom Anderson <twocs@users.noreply.github.com>
2020-05-24 15:49:51 +0930

    modify test: render Appointment in a component

--- test/Appointment.test.js
+++ test/Appointment.test.js
@@ -1,5 +1,12 @@
 describe('Appointment', () => {
   it('renders the customer\'s first name', () => {
+      const customer = { firstName: 'Tom' };
+      const component = <Appointment customer={customer} />;
+      
+      const container = document.createElement('div');
+      document.body.appendChild(container);
+      
+      ReactDOM.render(component, container);
       expect(document.body.textContent).toMatch('Tom');
   });
 });
